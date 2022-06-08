f487eedc6efb80c2943b2946206389a6e30b94be
ZJayee <jayee.zheng@gmail.com>
2022-06-08 01:10:03 -0700

    -Added equation image files -Created fomula viewer and explanation activity layout

---
+++ app/src/main/java/com/example/physicsdatareview/FormulaDisplayActivity.java
@@ -0,0 +1,14 @@
+package com.example.physicsdatareview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormulaDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_formula_display);
+    }
+}
--- app/src/main/java/com/example/physicsdatareview/MainActivity.java
+++ app/src/main/java/com/example/physicsdatareview/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.physicsdatareview;
 
-import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        TextView textView = findViewById(R.id.text);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), FormulaDisplayActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
